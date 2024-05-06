09b26aea36cfa5676865bf54f94c945888e4ae86
Yasser HADJ <129421498+y-hadj@users.noreply.github.com>
2024-05-06 16:48:05 +0100

    Add files via upload

---
+++ catkin_ws/build_isolated/multi_robot_scenario/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/zaki/DRL-robot-navigation/catkin_ws/devel_isolated/multi_robot_scenario:$CMAKE_PREFIX_PATH"
+export PWD='/home/zaki/DRL-robot-navigation/catkin_ws/build_isolated/multi_robot_scenario'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/zaki/DRL-robot-navigation/catkin_ws/devel_isolated/multi_robot_scenario/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/zaki/DRL-robot-navigation/catkin_ws/src/multi_robot_scenario:$ROS_PACKAGE_PATH"
